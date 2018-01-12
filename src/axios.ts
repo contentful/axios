0c09f778e3705b9fccd8094fd4ff21db17d5711a
Stephan Schneider <stephanschndr@gmail.com>
2018-01-12 13:48:15 +0100

    wip: axios TSLint fixes

--- src/axios.ts
+++ src/axios.ts
@@ -1,8 +1,8 @@
-'use strict';
+'use strict'
 
 import { extend, merge, bind } from 'lodash'
 import Axios from './core/Axios'
-var defaults = require('./defaults');
+import defaults from './defaults'
 
 interface MainExport {
   Axios?: any,
@@ -20,39 +20,39 @@ interface MainExport {
  * @param {Object} defaultConfig The default config for the instance
  * @return {Axios} A new instance of Axios
  */
-function createInstance(defaultConfig): MainExport {
-  var context = new Axios(defaultConfig);
-  var instance = bind(Axios.prototype.request, context);
+function createInstance (defaultConfig): MainExport {
+  let context = new Axios(defaultConfig)
+  let instance = bind(Axios.prototype.request, context)
 
   // Copy axios.prototype to instance
-  extend(instance, Axios.prototype, context);
+  extend(instance, Axios.prototype, context)
 
   // Copy context to instance
-  extend(instance, context);
+  extend(instance, context)
 
-  return instance as MainExport;
+  return instance as MainExport
 }
 
 // Create the default instance to be exported
-var axios = createInstance(defaults);
+let axios = createInstance(defaults)
 
 // Expose Axios class to allow class inheritance
-axios.Axios = Axios;
+axios.Axios = Axios
 
 // Factory for creating new instances
-axios.create = function create(instanceConfig) {
-  return createInstance(merge(defaults, instanceConfig));
-};
+axios.create = function create (instanceConfig) {
+  return createInstance(merge(defaults, instanceConfig))
+}
 
 // Expose Cancel & CancelToken
-axios.Cancel = require('./cancel/Cancel');
-axios.CancelToken = require('./cancel/CancelToken');
-axios.isCancel = require('./cancel/isCancel');
+axios.Cancel = require('./cancel/Cancel')
+axios.CancelToken = require('./cancel/CancelToken')
+axios.isCancel = require('./cancel/isCancel')
 
 // Expose all/spread
-axios.all = function all(promises) {
-  return Promise.all(promises);
-};
-axios.spread = require('./helpers/spread');
+axios.all = function all (promises) {
+  return Promise.all(promises)
+}
+axios.spread = require('./helpers/spread')
 
 export default axios

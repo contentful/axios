82ae5ccb172700808c31543d010a8d695f0984cd
Stephan Schneider <stephanschndr@gmail.com>
2018-01-26 15:17:58 +0100

    test: move specs/cancel

---
+++ test-migrated/specs/cancel/Cancel.spec.js
@@ -0,0 +1,15 @@
+import Cancel from '../../../src/cancel/Cancel'
+
+describe('Cancel', function () {
+  describe('toString', function () {
+    it('returns correct result when message is not specified', function () {
+      const cancel = new Cancel()
+      expect(cancel.toString()).toBe('Cancel')
+    })
+
+    it('returns correct result when message is specified', function () {
+      const cancel = new Cancel('Operation has been canceled.')
+      expect(cancel.toString()).toBe('Cancel: Operation has been canceled.')
+    })
+  })
+})
---
+++ test-migrated/specs/cancel/isCancel.spec.js
@@ -0,0 +1,12 @@
+import isCancel from '../../../src/cancel/isCancel'
+import Cancel from '../../../src/cancel/Cancel'
+
+describe('isCancel', function () {
+  it('returns true if value is a Cancel', function () {
+    expect(isCancel(new Cancel())).toBe(true)
+  })
+
+  it('returns false if value is not a Cancel', function () {
+    expect(isCancel({ foo: 'bar' })).toBe(false)
+  })
+})

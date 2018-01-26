0a1e8b5776e66a6a90cedc53a9768ab8ddcb4062
Stephan Schneider <stephanschndr@gmail.com>
2018-01-26 15:06:03 +0100

    test: move specs/utils

---
+++ test-migrated/specs/utils/isX.spec.js
@@ -0,0 +1,45 @@
+/* global isOldIE */
+
+import * as utils from '../../../src/utils'
+import * as Stream from 'stream'
+
+describe('utils::isX', function a () {
+  it('should validate ArrayBufferView', function b () {
+    // ArrayBuffer doesn't exist in IE8/9
+    if (isOldIE && typeof ArrayBuffer === 'undefined') {
+      return
+    }
+
+    console.log('dis is da utils', utils)
+
+    expect(utils.isArrayBufferView(new DataView(new ArrayBuffer(2)))).toEqual(true)
+  })
+
+  it('should validate FormData', function c () {
+    // FormData doesn't exist in IE8/9
+    if (isOldIE && typeof FormData === 'undefined') {
+      return
+    }
+
+    expect(utils.isFormData(new FormData())).toEqual(true)
+  })
+
+  it('should validate Blob', function d () {
+    // Blob doesn't exist in IE8/9
+    if (isOldIE && typeof Blob === 'undefined') {
+      return
+    }
+
+    expect(utils.isBlob(new Blob())).toEqual(true)
+  })
+
+  it('should validate Stream', function e () {
+    expect(utils.isStream(new Stream.Readable())).toEqual(true)
+    expect(utils.isStream({ foo: 'bar' })).toEqual(false)
+  })
+
+  it('should validate URLSearchParams', function f () {
+    expect(utils.isURLSearchParams(new URLSearchParams())).toEqual(true)
+    expect(utils.isURLSearchParams('foo=1&bar=2')).toEqual(false)
+  })
+})
